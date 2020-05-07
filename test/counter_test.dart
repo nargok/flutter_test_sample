2d0fe029106d6fa12b16f31e1921e2150ec624e2
Ryoichi Gokan <nargok@gmail.com>
2020-05-08 00:29:43 +0900

    group

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,9 +2,14 @@ import 'package:test/test.dart';
 import 'package:counterapp/counter.dart';
 
 void main() {
-  test('Counter value should be incremented', () {
-    final counter = Counter();
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+  });
 
+  test('value should be incremented', () {
+    final counter = Counter();
     counter.increment();
 
     expect(counter.value, 1);
